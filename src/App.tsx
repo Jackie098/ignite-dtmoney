9b4412ef4c26c4e2999b2ca6fbac7f4367e188bf
Carlos Augusto <carlosmiranda19122@gmail.com>
2022-11-24 08:48:39 -0300

    componentized the NewTransaction form and stylized

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Modal from "react-modal";
 
 import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement("#root");
 
@@ -26,12 +27,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      />
 
       <GlobalStyle />
     </>
