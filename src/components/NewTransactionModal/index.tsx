4755f41dd2bf86a0e112bae4066ef4874b431f7e
Carlos Augusto <carlosmiranda19122@gmail.com>
2022-11-25 20:40:45 -0300

    created context to Transactions and applied for all the code

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,11 +1,14 @@
+import { FormEvent, useContext, useState } from "react";
 import Modal from "react-modal";
-import { Container, RadioBox, TransactionTypeContainer } from "./styles";
+import { api } from "../../services/api";
+
+import { TransactionsContext } from "../../TransactionsContext";
 
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import closeIgm from "../../assets/close.svg";
-import { FormEvent, useState } from "react";
-import { api } from "../../services/api";
+
+import { Container, RadioBox, TransactionTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -16,17 +19,24 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext);
+
   const [title, setTitle] = useState("");
-  const [value, setValue] = useState(0);
-  const [category, setCategory] = useState(0);
+  const [amount, setAmount] = useState(0);
+  const [category, setCategory] = useState("");
   const [type, setType] = useState("deposit");
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    const data = { title, value, category, type };
+    await createTransaction({ title, amount, category, type });
 
-    api.post("/transactions", data);
+    setTitle("");
+    setAmount(0);
+    setCategory("");
+    setType("deposit");
+
+    onRequestClose();
   }
 
   return (
@@ -56,8 +66,8 @@ export function NewTransactionModal({
         <input
           type="number"
           placeholder="Valor"
-          value={value}
-          onChange={(event) => setValue(Number(event.target.value))}
+          value={amount}
+          onChange={(event) => setAmount(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
@@ -85,7 +95,7 @@ export function NewTransactionModal({
         <input
           placeholder="Categoria"
           value={category}
-          onChange={(event) => setCategory(Number(event.target.value))}
+          onChange={(event) => setCategory(event.target.value)}
         />
 
         <button type="submit">Cadastrar</button>
